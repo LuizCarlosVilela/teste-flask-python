31eb81cb2565f475d43986862b9a1cefb36bbcb4
LuizCarlosVilela <luizcarlosvilela@outlook.com.br>
2021-07-13 09:10:28 -0300

    -Criando carousel

--- app.py
+++ app.py
@@ -6,15 +6,10 @@ STATIC = './static'
 
 app = Flask(__name__, template_folder=TEMPLATES, static_folder=STATIC)
 
-@app.route('/')
-def helloWorld():
-    return 'Hello World!'
 
-@app.route('/home')
+@app.route('/')
 def home():
     data = datetime.datetime.now()
-    usuarios = ['Flávio', 'Isabella', 'João', 'Maria']
+    usuarios = ['Luiz', 'Amós', 'Arthur']
     mostrarUsuarios = True
     return render_template('home.html', dataAtual=data, usuarios=usuarios, mostrarUsuarios=mostrarUsuarios)
-
-#app.run(host='0.0.0.0', port=5000)
